2ea76405319d9d0071861151c99fc2450be19f3b
Jess Mitchell <jess@krit.com>
2023-03-10 14:40:37 -0500

    add user page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
-export const Card = ({name, id, url}) => {
-    return (
-        <div className="card">
-            <h2>{name}</h2>
-            <p>ID:{id}</p>
-            <p>URL: {url}</p>
-        </div>
-    )
-}
+export const Card = ({ name, id, url, object, ...props }) => {
+  console.log(props);
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+      <p>ID:{id}</p>
+      <p>Object type: {object}</p>
+      {url && <p>URL: {url}</p>}
+    </div>
+  );
+};
--- src/pages/users.js
+++ src/pages/users.js
@@ -1,10 +1,32 @@
 import Link from "next/link";
+import styles from "@/styles/Home.module.css";
+import { Client } from "@notionhq/client";
+import { Card } from "@/components/Card";
 
-export default function Users() {
+export default function Users({ users }) {
   return (
-    <div>
-      <Link href="/">Back</Link>
-      <h1>Users</h1>
-    </div>
+    <main className={styles.main}>
+      <div>
+        <Link href="/">Back</Link>
+        <h1>Users</h1>
+        <ul>
+          {users.map((u, i) => (
+            <Card key={i} {...u} />
+          ))}
+        </ul>
+      </div>
+    </main>
   );
+}
+
+export async function getStaticProps() {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const response = await notion.users.list();
+  console.log(response);
+  return {
+    props: {
+      users: response.results,
+    },
+    revalidate: 1,
+  };
 }
